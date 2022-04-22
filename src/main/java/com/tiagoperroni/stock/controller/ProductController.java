b36554b2a8933d67ff7997cf844da7ce2132fac1
Tiago <tiagoperroni86@gmail.com>
2022-04-22 16:08:14 -0300

    add findbyid endpoint

--- src/main/java/com/tiagoperroni/stock/controller/ProductController.java
+++ src/main/java/com/tiagoperroni/stock/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +26,16 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<List<Product>> getProducts() {
         return new ResponseEntity<>(productRepository.findAll(), HttpStatus.OK);
-    }
+    }    
 
     @PostMapping
     public ResponseEntity<Product> addProduct(@RequestBody Product product) {
         product.setCreatedAt(LocalDateTime.now());
         return new ResponseEntity<>(productRepository.save(product), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(productRepository.findById(id).orElse(null), HttpStatus.OK);
     }
 }
